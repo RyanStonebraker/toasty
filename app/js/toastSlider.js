8b1808dd754e463bdeacb1ead71c981d08692c2e
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-11-10 23:52:14 -0900

    Changes slider event to occur on slide for smoother transition

--- app/js/toastSlider.js
+++ app/js/toastSlider.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $("#burn-slider").slider();
 
   $( "#burn-slider" ).slider({
-    change: function( event, ui ) {
+    slide: function( event, ui ) {
       let burnAmount = $("#burn-slider").slider('value');
       $("section.breadholder img.toasted").css("opacity", burnAmount/100);
     }
